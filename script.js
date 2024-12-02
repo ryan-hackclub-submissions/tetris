0b5e2969c60214277b61fda56a72adfeb23b6200
ryan-hackclub-submissions <potatoslalom+0@gmail.com>
2024-12-01 21:48:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -189,9 +189,9 @@ document.addEventListener('keydown', function(event) {
   
         timeoutIds[event.key] = setTimeout(function() {
           // Repeat every
-          intervalIds[event.key] = setInterval(func, 75);
+          intervalIds[event.key] = setInterval(func, 70);
           // initial delay
-        }, 165);
+        }, 150);
       }
     }
 
